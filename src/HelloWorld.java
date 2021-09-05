56d05d02baae64b77b61d6b81ca960225ccbf372
AutomationBadri007 <svbadrinath07@gmail.com>
2021-09-05 17:42:08 +0530

    Added new print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld
 
 {
-    System.out.Println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World01");
+    }
 
 }
